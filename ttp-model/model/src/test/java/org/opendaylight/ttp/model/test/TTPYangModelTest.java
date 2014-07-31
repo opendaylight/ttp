1113e09e9b87309750d19988081a9b86c956bdc0
Colin Dixon <colin@colindixon.com>
2014-07-31 00:00:51 -0500

    Reworking DataObject => JSON String utils to correctly use DataSchemaNodes
    Change-Id: I3971f0d48412a9cb4cce0a592db4a77340468e83
    Signed-off-by: Colin Dixon <colin@colindixon.com>

--- ttp-model/model/src/test/java/org/opendaylight/ttp/model/test/TTPYangModelTest.java
+++ ttp-model/model/src/test/java/org/opendaylight/ttp/model/test/TTPYangModelTest.java
@@ -1,17 +1,19 @@
 package org.opendaylight.ttp.model.test;
 
-import static org.junit.Assert.assertNotNull;
-
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Set;
+
 import javassist.ClassPool;
+
+import javax.ws.rs.WebApplicationException;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.opendaylight.controller.sal.rest.impl.StructuredDataToJsonProvider;
 import org.opendaylight.controller.sal.rest.impl.StructuredDataToXmlProvider;
-import org.opendaylight.controller.sal.restconf.impl.ControllerContext;
-import org.opendaylight.controller.sal.restconf.impl.InstanceIdWithSchemaNode;
 import org.opendaylight.controller.sal.restconf.impl.StructuredData;
 import org.opendaylight.yang.gen.v1.urn.onf.ttp.rev140711.opendaylight.ttps.TableTypePatterns;
 import org.opendaylight.yang.gen.v1.urn.onf.ttp.rev140711.opendaylight.ttps.TableTypePatternsBuilder;
@@ -26,14 +28,15 @@ import org.opendaylight.yangtools.sal.binding.generator.impl.RuntimeGeneratedMap
 import org.opendaylight.yangtools.yang.binding.DataObject;
 import org.opendaylight.yangtools.yang.binding.YangModuleInfo;
 import org.opendaylight.yangtools.yang.binding.util.BindingReflections;
+import org.opendaylight.yangtools.yang.common.QName;
 import org.opendaylight.yangtools.yang.data.api.CompositeNode;
 import org.opendaylight.yangtools.yang.data.impl.codec.BindingIndependentMappingService;
 import org.opendaylight.yangtools.yang.data.impl.codec.xml.XmlDocumentUtils;
 import org.opendaylight.yangtools.yang.data.impl.codec.xml.XmlUtils;
 import org.opendaylight.yangtools.yang.model.api.DataNodeContainer;
 import org.opendaylight.yangtools.yang.model.api.DataSchemaNode;
-import org.opendaylight.yangtools.yang.model.api.GroupingDefinition;
 import org.opendaylight.yangtools.yang.model.api.SchemaContext;
+import org.opendaylight.yangtools.yang.model.api.SchemaContextListener;
 import org.w3c.dom.Document;
 import org.w3c.dom.bootstrap.DOMImplementationRegistry;
 import org.w3c.dom.ls.DOMImplementationLS;
@@ -117,12 +120,10 @@ public class TTPYangModelTest {
         /*
          * test parameters slug JSON conversion
          */
-		Parameters p = new ParametersBuilder()
-				.setDoc(Arrays.asList("documentation"))
-				.setName("Showing-curt-how-this-works").setType("type1")
-				.build();
-		String expectedStr = "\"parameters\":{\"doc\":\"documentation\",\"type\":\"type1\"}";
-		testJsonConversion(expectedStr, p);
+        Parameters p = new ParametersBuilder().setDoc(Arrays.asList("documentation"))
+                .setName("Showing-curt-how-this-works").setType("type1").build();
+        String expectedStr = "\"parameters\":{\"doc\":\"documentation\",\"type\":\"type1\"}";
+        testJsonConversion(expectedStr, p);
 
         /*
          * test variables
@@ -132,69 +133,61 @@ public class TTPYangModelTest {
                 .setName("Showing-curt-how-this-works").build();
         Variables p2 = new VariablesBuilder().setDoc(Arrays.asList("documentation"))
                 .setName("Showing-curt-how-this-works").build();
-        t.setVariables(Arrays.asList(p1));
+        t.setVariables(Arrays.asList(p1, p2));
         TableTypePatterns builtTTP = t.build();
         testJsonConversion(expectedStr, builtTTP);
         System.out.println(builtTTP.getVariables().getClass().getName());
     }
 
-    private Document documentFromDataObject(DataObject data) {
-        return documentFromCompositeNode(compositeNodeFromDataObject(data));
-    }
+    /**
+     * DON'T CALL THIS IN PRODUCTION CODE EVER!!! UNTIL IT IS FIXED!
+     * <p/>
+     * Return the {@link DataSchemaNode}
+     *
+     * @param context
+     * @param d
+     */
+    public DataSchemaNode getSchemaNodeForDataObject(SchemaContext context, DataObject d) {
+        // let's assume I can get this
+        //QName qn = mappingService.registry.serialize(d.getClass());
+        QName qn = BindingReflections.findQName(d.getClass());
 
-    private Document documentFromCompositeNode(CompositeNode compNode) {
-        return XmlDocumentUtils.toDocument(compNode, XmlUtils.DEFAULT_XML_CODEC_PROVIDER);
-    }
+        System.out.println("d.qn: "+qn);
 
-    private CompositeNode compositeNodeFromDataObject(DataObject d) {
-        return mappingService.toDataDom(d);
-    }
+        Set<DataSchemaNode> allTheNodes = getAllTheNode(context);
 
-    private StructuredData structuredDataFromCompositeNode(CompositeNode compNode) {
-        // Create structured data, the null is an unused mountpoint
-        // final true/false is to turn on/off pretty printing
-        DataSchemaNode NDM_metadata = null;
-        //first solution
-        NDM_metadata = findSchemaForTableTypePattern(context);
-        //second solution
-        ControllerContext controllerContext = ControllerContext.getInstance();
-        controllerContext.setSchemas(context);
-        InstanceIdWithSchemaNode iiAndSchema = controllerContext.toInstanceIdentifier("/onf-ttp:opendaylight-ttps/table-type-patterns/NDM_metadata");
-        NDM_metadata = iiAndSchema.getSchemaNode();
 
-        return new StructuredData(compNode, NDM_metadata, null, true);
-    }
-
-    private DataSchemaNode findSchemaForTableTypePattern(SchemaContext context) {
-        Set<GroupingDefinition> groupings = context.getGroupings();
-        DataNodeContainer tableTypePatternGrouping = null;
-        for (GroupingDefinition grouping : groupings) {
-            if (grouping.getQName().getLocalName().equals("table-type-pattern")) {
-                tableTypePatternGrouping = grouping;
-                break;
+        // TODO: create a map to make this faster!!!!
+        for ( DataSchemaNode dsn : allTheNodes ) {
+            if(dsn instanceof DataNodeContainer) {
+                allTheNodes.addAll(((DataNodeContainer)dsn).getChildNodes());
+            }
+            System.out.println("    >>>> "+dsn.getQName());
+            if (dsn.getQName().equals(qn)) {
+                return dsn;
             }
         }
-        assertNotNull(tableTypePatternGrouping);
-        DataSchemaNode nDMMetadataContainer = tableTypePatternGrouping.getDataChildByName("NDM_metadata");
-        assertNotNull(nDMMetadataContainer);
-
-        return nDMMetadataContainer;
+        return null;
     }
 
-    private StructuredData structuredDataFromDataObject(DataObject d) {
-        return structuredDataFromCompositeNode(compositeNodeFromDataObject(d));
+    public Set<DataSchemaNode> getAllTheNode(SchemaContext context) {
+        Set<DataSchemaNode> nodes = new HashSet<DataSchemaNode>();
+        getAllTheNodesHelper(context, nodes);
+        return nodes;
+    }
+
+    private void getAllTheNodesHelper(DataNodeContainer dcn, Set<DataSchemaNode> nodes) {
+        for ( DataSchemaNode dsn : dcn.getChildNodes() ) {
+            if (dsn instanceof DataNodeContainer){
+                getAllTheNodesHelper((DataNodeContainer)dsn, nodes);
+            }
+            nodes.add(dsn);
+        }
     }
 
     private void testJsonConversion(String expectedJson, DataObject data) throws Exception {
-        StructuredData d = structuredDataFromDataObject(data);
-
-        ByteArrayOutputStream s = new ByteArrayOutputStream();
-        // ignore all the nulls
-        StructuredDataToJsonProvider.INSTANCE.writeTo(d, null, null, null, null, null, s);
-        System.out.println("Formatted JSON from "
-                + StructuredDataToJsonProvider.class.getName() + ":");
-        String structuredDataJSON = s.toString();
-        System.out.println(structuredDataJSON);
+        String jsonString = jsonStringFromDataObject(data);
+        System.out.println(jsonString);
 
         // TODO: get simple JSON parser to do JSON equality
         //Assert.assertEquals(expectedJson, structuredDataJSON);
@@ -218,5 +211,118 @@ public class TTPYangModelTest {
         final LSSerializer writer = impl.createLSSerializer();
         writer.getDomConfig().setParameter("format-pretty-print", Boolean.TRUE);
         return writer.writeToString(document);
+    }
+
+    /*
+     * Helper functions around converting between DataObjects, CompositeNodes,
+     * Documents, StructuredData, and JSON strings
+     */
+
+    /**
+     *
+     * @param data
+     * @return
+     */
+    public Document documentFromDataObject(DataObject data) {
+        return documentFromCompositeNode(compositeNodeFromDataObject(data));
+    }
+
+    /**
+     *
+     * @param compNode
+     * @return
+     */
+    public Document documentFromCompositeNode(CompositeNode compNode) {
+        return XmlDocumentUtils.toDocument(compNode, XmlUtils.DEFAULT_XML_CODEC_PROVIDER);
+    }
+
+    /**
+     *
+     * @param d
+     * @return
+     */
+    public CompositeNode compositeNodeFromDataObject(DataObject d) {
+        return mappingService.toDataDom(d);
+    }
+
+    /**
+     *
+     * @param compNode
+     * @return
+     */
+//    private StructuredData structuredDataFromCompositeNode(CompositeNode compNode) {
+//        // Create structured data, the null is an unused mountpoint
+//        // final true/false is to turn on/off pretty printing
+//        DataSchemaNode NDM_metadata = null;
+//        //first solution
+//        NDM_metadata = getSchemaNodeForDataObject(context, null);
+//        //second solution
+//        ControllerContext controllerContext = ControllerContext.getInstance();
+//        controllerContext.setSchemas(context);
+//        InstanceIdWithSchemaNode iiAndSchema = controllerContext.toInstanceIdentifier("/onf-ttp:opendaylight-ttps/table-type-patterns/table-type-pattern/NDM_metadata");
+//        NDM_metadata = iiAndSchema.getSchemaNode();
+//
+//        return new StructuredData(compNode, NDM_metadata, null, true);
+//    }
+
+    /**
+     *
+     * @param d
+     * @return
+     */
+    public StructuredData structuredDataFromDataObject(DataObject d) {
+        DataSchemaNode NDM_metadata = null;
+        NDM_metadata = getSchemaNodeForDataObject(context, d);
+        return new StructuredData(compositeNodeFromDataObject(d), NDM_metadata, null, true);
+    }
+
+    /**
+     *
+     * @param d
+     * @return
+     * @throws WebApplicationException
+     * @throws IOException
+     */
+    public String jsonStringFromStructuredData(StructuredData d) throws WebApplicationException, IOException{
+        ByteArrayOutputStream s = new ByteArrayOutputStream();
+        StructuredDataToJsonProvider.INSTANCE.writeTo(d, null, null, null, null, null, s);
+        return s.toString();
+    }
+
+    /**
+     *
+     * @param cn
+     * @return
+     * @throws WebApplicationException
+     * @throws IOException
+     */
+//    public String jsonStringFromCompositeNode(CompositeNode cn) throws WebApplicationException, IOException{
+//        return jsonStringFromStructuredData(structuredDataFromCompositeNode(cn));
+//    }
+
+
+    /**
+     * I want this:
+     *   DataObject ==???===> JSON String
+     *
+     * To do that, you hve to do this:
+     *   DataObject => CompositeNode => StructuredData => JSON String
+     *                                       |
+     *            SchemaContext ==??==> DataNodeSchema
+     */
+
+    /**
+     * Converts a {@link DataObject} to a JSON representation in a string using the relevant YANG
+     * schema if it is present. This defaults to using a {@link SchemaContextListener} if running an
+     * OSGi environment or {@link BindingReflections#loadModuleInfos()} if run while not in an OSGi
+     * environment or if the schema isn't available via {@link SchemaContextListener}.
+     *
+     * @param d
+     * @return
+     * @throws WebApplicationException
+     * @throws IOException
+     */
+    public String jsonStringFromDataObject(DataObject d) throws WebApplicationException, IOException{
+        return jsonStringFromStructuredData(structuredDataFromDataObject(d));
     }
 }
