44e1c547d18f76f0b9a0596b6fc61e667e3556fd
Colin Dixon <colin@colindixon.com>
2015-01-19 19:40:09 -0600

    Adding first take at parser as an executable jar
    After building the parser bundle, You can now run:
    java -jar target/parser-0.1.0-SNAPSHOT-jar-with-dependencies.jar
    
    Change-Id: Ib8d58b2ef362fcf693ac537889a1212e60886704
    Signed-off-by: Colin Dixon <colin@colindixon.com>

---
+++ parser/src/main/java/org/opendaylight/ttp/parser/Main.java
@@ -0,0 +1,10 @@
+package org.opendaylight.ttp.parser;
+
+//import org.opendaylight.ttp.model.test.TTPYangModelTest;
+
+public class Main {
+  public static void main(String args[]) {
+      //TTPYangModelTest.main(args);
+      System.out.println("Does this work?");
+  }
+}
