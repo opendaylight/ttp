dabbdac9fbb2968ff0aae4149d74666e4a8db26e
Curt Beckmann <beckmann@brocade.com>
2015-04-09 11:02:13 -0400

    Added more cli command support
    Implemented validate (prep for format, compare, dot)
    Also added code to read TTP from a file, some Javadoc
    
    Change-Id: I2bc283fd9b5145abaf857e242209b311ad10dc13
    Signed-off-by: Colin Dixon <colin@colindixon.com>
    Signed-off-by: Curt Beckmann <beckmann@brocade.com>

--- parser/src/main/java/org/opendaylight/ttp/parser/Main.java
+++ parser/src/main/java/org/opendaylight/ttp/parser/Main.java
@@ -5,7 +5,8 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
-import java.io.IOException;
+import java.io.*;
+//import java.io.IOException;
 import java.util.Arrays;
 import java.util.Collections;
 import javax.ws.rs.WebApplicationException;
@@ -20,6 +21,19 @@ import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 import org.opendaylight.yangtools.yang.binding.util.BindingReflections;
 import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
 
+/**
+ * Parser main function for command line manipulation of TTP files
+ * <p>
+ * Behavior depends on first argument
+ * help                            // print this message");
+ * format <ttp-file>               // pretty print the ttp to stdout");
+ * validate <ttp-file>             // check the syntax of the ttp");
+ * compare <ttp-file1> <ttp-file2> // check 2 ttps for common flow paths");
+ * dot <ttp-file>                  // generate dot formatted table map graphic to stdout");
+ * <p>
+ * @author curtbeckmann
+ *
+ */
 public class Main {
 
     public static void main(String args[]) throws WebApplicationException, IOException {
@@ -29,20 +43,66 @@ public class Main {
         try {
             ttpUtils = new TTPUtils(Collections.singleton(BindingReflections.getModuleInfo(OpendaylightTtps.class)));
         } catch (Exception e) {
+            System.out.println("Exception!");
             throw Throwables.propagate(e);
         }
 
+        if (args.length > 0) {
+            switch (args[0]) {
+            default:
+            case "help":
+                System.out.println("<cmd> syntax:");
+                System.out.println("<cmd> help                            // print this message");
+                System.out.println("<cmd> format <ttp-file>               // pretty print the ttp to stdout");
+                System.out.println("<cmd> validate <ttp-file>             // check the syntax of the ttp");
+                System.out.println("<cmd> compare <ttp-file1> <ttp-file2> // check 2 ttps for common flow paths");
+                System.out.println("<cmd> dot <ttp-file>                  // generate dot formatted table map graphic to stdout");
+                break;
+            case "validate":
+                if (args.length != 2) {
+                    System.out.println("<cmd> validate <ttp-file>                    // check the syntax of the ttp");    
+                }
+                else {
+                    System.out.println("About to stream in the ttp file: "+ args[1]);
+                    BufferedReader d = new BufferedReader(new
+                            InputStreamReader(new FileInputStream(args[1])));
+                    String count;
+                    String ttpCore = "";
+                    while((count = d.readLine()) != null){
+                        ttpCore = ttpCore.concat(count);
+                    }
+                    d.close();
 
-        String ttpToParse = "{\"onf-ttp:opendaylight-ttps\": {\"table-type-patterns\": {\"table-type-pattern\": [ {\"NDM_metadata\": {\"authority\": \"org.opennetworking.fawg\",\"type\": \"TTPv1\",\"name\": \"L2-L3-ACLs\",\"version\": \"1.0.0\",\"OF_protocol_version\": \"1.3.3\",\"doc\": [\"Example of a TTP supporting L2 (unicast, multicast, flooding), L3 (unicast only),\", \"and an ACL table.\"]}}]}}}";
-        NormalizedNode<?, ?> n = ttpUtils.normalizedNodeFromJsonString(ttpToParse);
-        DataObject dataObj = ttpUtils.dataObjectFromNormalizedNode(n);
-        if(dataObj instanceof OpendaylightTtps){
-            System.out.println("It's a TTP!");
-            String ttpInJSON = ttpUtils.jsonStringFromDataObject(
-                    InstanceIdentifier.create(OpendaylightTtps.class), dataObj);
-            System.out.println(ttpInJSON);
-        }else{
-            System.out.println("Bummer, not a TTP!");
+                    String ttpPrepend = "{\"onf-ttp:opendaylight-ttps\": {\"table-type-patterns\": {\"table-type-pattern\": [";
+                    String ttpPostpend = "]}}}";
+                    String ttpToParse =  ttpPrepend + ttpCore + ttpPostpend;
+                    System.out.println("ttpToParse is:");
+                    System.out.println(ttpToParse);
+                    DataObject dataObj;
+                    try {
+                        NormalizedNode<?, ?> n = ttpUtils.normalizedNodeFromJsonString(ttpToParse);
+                        System.out.println("Created the NormalizedNode");
+                        dataObj = ttpUtils.dataObjectFromNormalizedNode(n);
+                    } catch (Exception e) {
+                        System.out.println("Exception creating DataObject from JSON string!");
+                        throw Throwables.propagate(e);
+                    }
+                    if(dataObj instanceof OpendaylightTtps){
+                        System.out.println("It's a TTP!");
+                        String ttpInJSON = ttpUtils.jsonStringFromDataObject(
+                                InstanceIdentifier.create(OpendaylightTtps.class), dataObj);
+                        System.out.println(ttpInJSON);
+                    }else{
+                        System.out.println("Bummer, not a TTP!");
+                    }
+                }
+                break;
+            case "format":
+            case "compare":
+            case "dot":
+                System.out.println(args[0]+" not yet written");
+                break;
+            }
         }
 
         String expectedJson = "{\"NDM_metadata\": {\"authority\": \"org.opennetworking.fawg\",\"type\": \"TTPv1\",\"name\": \"L2-L3-ACLs\",\"version\": \"1.0.0\",\"OF_protocol_version\": \"1.3.3\",\"doc\": [\"Example of a TTP supporting L2 (unicast, multicast, flooding), L3 (unicast only),\", \"and an ACL table.\"]}}";
