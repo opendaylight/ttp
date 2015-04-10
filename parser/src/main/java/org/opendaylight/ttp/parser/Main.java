cd8f3e9e429a327287601c842d08fc3b528d1ca9
Colin Dixon <colin@colindixon.com>
2015-04-09 20:57:27 -0400

    Add utility functions to go from json to DataObjects
    Change-Id: I3854d056d58dbd91017c62594e2f6a44e9e6f345
    Signed-off-by: Colin Dixon <colin@colindixon.com>

--- parser/src/main/java/org/opendaylight/ttp/parser/Main.java
+++ parser/src/main/java/org/opendaylight/ttp/parser/Main.java
@@ -15,8 +15,10 @@ import org.opendaylight.yang.gen.v1.urn.onf.ttp.rev140711.opendaylight.ttps.Tabl
 import org.opendaylight.yang.gen.v1.urn.onf.ttp.rev140711.opendaylight.ttps.table.type.patterns.TableTypePattern;
 import org.opendaylight.yang.gen.v1.urn.onf.ttp.rev140711.table.type.pattern.properties.NDMMetadata;
 import org.opendaylight.yang.gen.v1.urn.onf.ttp.rev140711.table.type.pattern.properties.NDMMetadataBuilder;
+import org.opendaylight.yangtools.yang.binding.DataObject;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 import org.opendaylight.yangtools.yang.binding.util.BindingReflections;
+import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
 
 public class Main {
 
@@ -30,6 +32,18 @@ public class Main {
             throw Throwables.propagate(e);
         }
 
+
+        String ttpToParse = "{\"onf-ttp:opendaylight-ttps\": {\"table-type-patterns\": {\"table-type-pattern\": [ {\"NDM_metadata\": {\"authority\": \"org.opennetworking.fawg\",\"type\": \"TTPv1\",\"name\": \"L2-L3-ACLs\",\"version\": \"1.0.0\",\"OF_protocol_version\": \"1.3.3\",\"doc\": [\"Example of a TTP supporting L2 (unicast, multicast, flooding), L3 (unicast only),\", \"and an ACL table.\"]}}]}}}";
+        NormalizedNode<?, ?> n = ttpUtils.normalizedNodeFromJsonString(ttpToParse);
+        DataObject dataObj = ttpUtils.dataObjectFromNormalizedNode(n);
+        if(dataObj instanceof OpendaylightTtps){
+            System.out.println("It's a TTP!");
+            String ttpInJSON = ttpUtils.jsonStringFromDataObject(
+                    InstanceIdentifier.create(OpendaylightTtps.class), dataObj);
+            System.out.println(ttpInJSON);
+        }else{
+            System.out.println("Bummer, not a TTP!");
+        }
 
         String expectedJson = "{\"NDM_metadata\": {\"authority\": \"org.opennetworking.fawg\",\"type\": \"TTPv1\",\"name\": \"L2-L3-ACLs\",\"version\": \"1.0.0\",\"OF_protocol_version\": \"1.3.3\",\"doc\": [\"Example of a TTP supporting L2 (unicast, multicast, flooding), L3 (unicast only),\", \"and an ACL table.\"]}}";
         InstanceIdentifier<NDMMetadata> metaPath = InstanceIdentifier.create(OpendaylightTtps.class)
